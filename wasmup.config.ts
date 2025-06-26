6cf5307c764cc49e97aac2205cd7927e7fe2639f
rainbowatcher <rainbow-w@qq.com>
2025-06-26 15:03:07 +0800

    chore: remove entry point and release flag configurations
    - Remove the entry point configuration from the project setup
    - Delete the release flag from the configuration options
    
    Signed-off-by: rainbowatcher <rainbow-w@qq.com>

--- wasmup.config.ts
+++ wasmup.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "wasmup"
 
 export default defineConfig({
     clean: true,
-    entry: ["."],
     output: "packages/toml-edit-js",
-    release: true,
     scope: "rainbowatcher",
 })
