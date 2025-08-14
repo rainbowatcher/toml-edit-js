e09e01bec5c1c0d1dfda59f30086219952084a34
rainbowatcher <rainbow-w@qq.com>
2025-08-14 17:56:23 +0800

    chore: remove support for legacy toml-edit-js versions
    - Removed import of version 0.2 from the toml-edit-js library
    - Preserved initialization and tests for versions 0.3, 0.4, and 0.5
    - Commented out the test for version 0.2 to exclude it from execution
    
    Signed-off-by: rainbowatcher <rainbow-w@qq.com>

--- bench/edit.bench.ts
+++ bench/edit.bench.ts
@@ -1,5 +1,4 @@
 import fs from "node:fs/promises"
-import v2init, { edit as v2edit } from "@rainbowatcher/toml-edit-js@v0.2"
 import v3init, { edit as v3edit } from "@rainbowatcher/toml-edit-js@v0.3"
 import v4init, { edit as v4edit } from "@rainbowatcher/toml-edit-js@v0.4"
 import v5init, { edit as v5edit } from "@rainbowatcher/toml-edit-js@v0.5"
@@ -7,17 +6,12 @@ import { bench } from "vitest"
 import init, { edit } from "../packages/toml-edit-js/shims"
 
 
-await v2init()
 await v3init()
 await v4init()
 await v5init()
 await init()
 const toml = await fs.readFile(`${process.cwd()}/bench/fixture/pyproject.toml`, "utf8")
 
-
-bench("v0.2", () => {
-    v2edit(toml, "flake8.ignore", "W500")
-})
 
 bench("v0.3", () => {
     v3edit(toml, "flake8.ignore", "W500")
